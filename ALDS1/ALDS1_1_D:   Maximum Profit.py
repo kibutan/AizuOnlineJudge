10443239d7853f76a96d9b628e181b654e3b4cea
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2021-01-06 08:52:53 +0900

    Update ALDS1_1_D:   Maximum Profit.py
    AC

--- ALDS1/ALDS1_1_D:   Maximum Profit.py
+++ ALDS1/ALDS1_1_D:   Maximum Profit.py
@@ -1,11 +1,18 @@
 n = int(input())
 a = [int(input()) for _ in range(n)]
-ans = -1000000000
-tmp = []
-for i in range(n-1):
-  for j in range(i+1,n):
-    tmp.append(a[j])
-    tmp = list(set(tmp))
-  ans = max(ans,(max(tmp)-a[i]))
-  tmp=[]
-print(ans)
+#ans = -1000000000
+#tmp = []
+#for i in range(n-1):
+##  for j in range(i+1,n):
+#    tmp.append(a[j])
+#    tmp = list(set(tmp))
+#  ans = max(ans,(max(tmp)-a[i]))
+#  tmp=[]
+#print(ans)
+min = a[0]
+profit = max(a[1:])-a[0] 
+for i in range(1,n-1):
+  if(min > a[i]):
+    min = a[i]
+    profit = max(profit,max(a[i+1:]) - a[i])  
+print(profit)
