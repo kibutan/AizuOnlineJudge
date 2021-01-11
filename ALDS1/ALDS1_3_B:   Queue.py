e17c206cbb20fd9e19ae679126c3377fe64db2ef
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2021-01-11 11:36:53 +0900

    Update ALDS1_3_B:   Queue.py

--- ALDS1/ALDS1_3_B:   Queue.py
+++ ALDS1/ALDS1_3_B:   Queue.py
@@ -6,17 +6,17 @@ process = deque([list(map(str,input().split())) for _ in range (n)])
 #for i in range(2):
 #  print("i",i)
 while process != deque([]):
-  print("before:",process)
-  print("remain time of task:", int(process[0][1]))
+#  print("before:",process)
+#  print("remain time of task:", int(process[0][1]))
   if(int(process[0][1]) <= q):
-    print("less q")
+#    print("less q")
     current_time = current_time + int(process[0][1])
     print(process[0][0],current_time)
     process.popleft()
   else:
-    print("more q")
+#    print("more q")
     current_time = current_time + q
     process.append([process[0][0],str(int(process[0][1])-q)])
     process.popleft()
-  print("after:",process)
-  print(current_time)
+#  print("after:",process)
+#  print(current_time)
