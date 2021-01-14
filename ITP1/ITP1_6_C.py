6280b84996d8bc1e9ded80cbade1c990ccee7e83
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2021-01-14 10:11:10 +0900

    Create ITP1_6_C.py
    WiP

---
+++ ITP1/ITP1_6_C.py
@@ -0,0 +1,17 @@
+n = int(input())
+b,f,r,v = list(map(int,input().split()))
+#for k in range(3):
+#  for i in range(3):
+#    for j in range(10):
+#      
+#      print("",0, end="")
+#    print()
+#  print("####################")
+
+for k in range(3):
+  for i in range(3):
+    for j in range(10):
+      ans.append(0)
+      print("",0, end="")
+    print()
+  print("####################")
