bf1f379505a28b352c51b768d7b9917f8bf1b659
木伏 徹 <46431930+kibutan@users.noreply.github.com>
2021-05-26 10:16:25 +0900

    Update ALDS1_11_B:   Depth First Search.py
    AC

--- ALDS1/ALDS1_11_B:   Depth First Search.py
+++ ALDS1/ALDS1_11_B:   Depth First Search.py
@@ -7,21 +7,26 @@ f = [0]*(n+1)
 d = [0]*(n+1)
 d[1] = time
 stack = deque([1])
-
-while(stack):
-    s = stack[0]
-    k = ukv[s][1]
-    if k == 0:
-        v = stack.popleft()
+for i in range(1,n+1):
+    if i not in arrive:
+        stack.appendleft(i)
+        arrive.appendleft(i)
         time += 1
-        f[v] = time
-    else:
-        v=ukv[s].pop(2)
-        # stack.appendleft(ukv[s][2])
-        ukv[s][1] -= 1
-        if(v not in arrive):
-            stack.appendleft(v)
-            arrive.append(v)
-            time += 1
-            d[v] = time
+        d[i] = time
+        while(stack):
+            s = stack[0]
+            k = ukv[s][1]
+            if k == 0:
+                v = stack.popleft()
+                time += 1
+                f[v] = time
+            else:
+                v=ukv[s].pop(2)
+                # stack.appendleft(ukv[s][2])
+                ukv[s][1] -= 1
+                if(v not in arrive):
+                    stack.appendleft(v)
+                    arrive.append(v)
+                    time += 1
+                    d[v] = time
 for i in range(1,n+1):print(i,d[i],f[i])
